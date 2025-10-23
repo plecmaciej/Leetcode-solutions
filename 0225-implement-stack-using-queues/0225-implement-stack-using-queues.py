1c64d92d39d975e9da5656158bc7d16138f2b29d
plecmaciej <plecmaciej2112@gmail.com>
2025-10-23 18:16:49 +0200

    Time: 0 ms (100%), Space: 18.4 MB (22.22%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,35 @@
+from queue import Queue
+class MyStack:
+
+    def __init__(self):
+        self.q1 = Queue()
+        self.q2 = Queue()
+
+    def push(self, x: int) -> None:
+        while not self.q1.empty():
+            self.q2.put(self.q1.get())
+        self.q1.put(x)
+        while not self.q2.empty():
+            self.q1.put(self.q2.get())
+
+
+    def pop(self) -> int:
+        if self.q1.empty():
+            raise IndexError("pop from empty stack")
+        return self.q1.get()
+
+    def top(self) -> int:
+        if self.q1.empty():
+            raise IndexError("top from empty stack")
+        return self.q1.queue[0]
+
+    def empty(self) -> bool:
+        return self.q1.empty()
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
