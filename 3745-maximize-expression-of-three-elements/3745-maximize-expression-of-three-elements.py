925835651b8a7e46e97740fc5da05b55bc263527
plecmaciej <plecmaciej2112@gmail.com>
2026-02-24 22:43:26 +0100

    Time: 14 ms (5.43%), Space: 19.4 MB (42.55%) - LeetHub

---
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximizeExpressionOfThree(self, nums: List[int]) -> int:
+        a = -101
+        b = -101
+        c = nums[0]
+        for num in nums:
+            
+            if num > b:
+                if num > a:
+                    if b != -101:
+                        if b < c:
+                            c = b
+                    a, b = num, a
+                else:
+                    if b != -101:
+                        if b < c:
+                            b,c = num,b
+                        else:
+                            b = num
+                    else:
+                        b = num
+            elif num < c:
+                c = num
+            print(a,b,c)
+        
+        return a+b-c
